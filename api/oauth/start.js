bf3d72e64490d8462189f0db44ebe494f3c1fa8b
mycologyus <mycologyus@gmail.com>
2025-12-24 22:04:03 -0500

    Create start.js

---
+++ api/oauth/start.js
@@ -0,0 +1,11 @@
+module.exports = (req, res) => {
+  const params = new URLSearchParams({
+    response_type: "code",
+    client_id: process.env.OURA_CLIENT_ID,
+    redirect_uri: process.env.OURA_REDIRECT_URI,
+    scope: "daily sleep readiness activity",
+  });
+
+  const url = `https://cloud.ouraring.com/oauth/authorize?${params.toString()}`;
+  res.redirect(url);
+};
